b8c9d9ab2898b1152017ac93a203ed75f40ff58e
ConradoIT <conradobasso@gmail.com>
2018-07-10 12:08:41 -0300

    Implementando Connexão

---
+++ RemoteDba/RemoteDba.Terminal/Configuration/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace RemoteDba.Terminal.Configuration
+{
+    public static class Manager
+    {
+        private static IConfiguration _configuration => LoadConfiguration();
+
+        private static IConfigurationRoot LoadConfiguration()
+        {
+            return new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                              .AddJsonFile("appsetings.json").Build();
+        }
+
+        public static string ConnectionString()
+        {
+            return _configuration["stringConnetion"];
+        }
+    }
+}
---
+++ RemoteDba/RemoteDba.Terminal/DataAcess/Connection.cs
@@ -0,0 +1,31 @@
+﻿using RemoteDba.Terminal.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace RemoteDba.Terminal.DataAcess
+{
+    public class Connection : IDisposable
+    {
+        private readonly SqlConnection _sqlConnetion;
+        private readonly StringBuilder _sql;
+        private readonly List<SqlParameter> _parameters;
+
+        public Connection ()
+        {
+            _sqlConnetion = new SqlConnection(Manager.ConnectionString());
+            _sqlConnetion.Open();
+            _sql = new StringBuilder();
+            _parameters = new List<SqlParameter>();
+        }
+
+        public StringBuilder SQL { get => _sql; }
+        public List<SqlParameter> Parameters { get => _parameters; }
+
+        public void Dispose()
+        {
+            _sqlConnetion.Dispose();
+        }
+    }
+}
